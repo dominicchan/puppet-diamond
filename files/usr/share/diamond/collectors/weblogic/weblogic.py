d4aa20656702b853ee3dbd6aac328d4411fbd54e
Dominic Chan <chan@pythian.com>
2014-12-24 21:12:15 +0000

    initial drop of weblogic collector

---
+++ files/usr/share/diamond/collectors/weblogic/weblogic.py
@@ -0,0 +1,25 @@
+import diamond.collector
+import sys
+import subprocess
+import re
+import string
+
+class WeblogicCollector(diamond.collector.Collector):
+
+    def get_default_config(self):
+        """
+        Returns the default collector settings
+        """
+        config = super(WeblogicCollector, self).get_default_config()
+        config.update({
+            'enabled':  False,
+        })
+        return config
+
+    def collect(self):
+        p = subprocess.Popen([self.config['wsltpath'], self.config['wsltscript']], env={'JAVA_HOME':self.config['java_home']}, stdout=subprocess.PIPE)
+        for line in iter(p.stdout.readline,''):
+            if re.match("^metric:", line) is not None:
+                label, metric_name, shortname, metric_value = string.split(line.rstrip(),":")
+                # Publish Metric
+                self.publish(metric_name, metric_value)
